c52e0df7cd331e718b53038fdeb16d025ec0f788
deppong <30512867+deppong@users.noreply.github.com>
2022-11-15 05:20:55 -0500

    added the particle struct to it's own file. implemented rotation around the x axis through rotation matrix

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate sdl2;
 extern crate rand;
 
+use std::f32::consts::PI;
 use std::{time::*, thread};
 use rand::Rng;
+
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
@@ -24,7 +26,7 @@ fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("sdlrs_template", WIDTH, HEIGHT).position_centered().build().unwrap();
+    let window = video_subsystem.window("Lorenz Attractor", WIDTH, HEIGHT).resizable().position_centered().build().unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
 
@@ -42,15 +44,17 @@ fn main() {
     let rho = 28.0;
     let sigma = 10.0;
     let beta = 8.0 / 3.0;
-    let mut dt = 0.001;
+    let dt = 0.001;
 
     let scale: f32 = 10.0;
 
-    let P_COUNT = 100;
+    let particle_count = 10000;
+
+    let mut angle: f32 = 0.0;
 
     let mut particles: Vec<particle> = vec![];
 
-    for i in 0..P_COUNT {
+    for i in 0..particle_count {
         let mut rng = rand::thread_rng();
         particles.push(particle::new(i as f32, Color::RGB(rng.gen(), rng.gen(), rng.gen()) ));
     }
@@ -68,6 +72,8 @@ fn main() {
                 _ => {}
             }
         }
+
+        framedata = vec![0; ((WIDTH*HEIGHT)*4) as usize];
         
         // edit physics
         for mut i in &mut particles {
@@ -83,17 +89,28 @@ fn main() {
 
 
         for i in &mut particles {
-            // put_pixel(i.x as u32, i.y as u32, Color::WHITE, &mut framedata);
-            let x = (i.x * scale) + WIDTH as f32/2.0;
-            let y = (i.y * scale) + HEIGHT as f32/2.0;
+            /*
+                x axis rotation matrix
+                [ 1       0           0     ]
+                [ 0 angle.cos() -angle.sin()] 
+                [ 0 angle.sin()  angle.cos()]
+            */
+            let matx = i.x * 1.0 + i.y * 0.0 + i.z * 0.0;
+            let maty = i.x * 0.0 + i.y * angle.cos() + i.z * -angle.sin();
+
+            let x = (matx * scale) + WIDTH as f32/2.0;
+            let y = (maty * scale) + HEIGHT as f32/2.0;
+
             if x >= WIDTH as f32 || y >= HEIGHT as f32 || x <= 0.0 || y <= 0.0 { continue; }
             put_pixel(x as u32, y as u32, i.color, &mut framedata);
         }
+
 
         if Instant::now() - last_time < Duration::from_secs_f32(1.0 * dt) {
             thread::sleep(Duration::from_secs_f32(1.0 * dt) - (Instant::now() - last_time));
             last_time = Instant::now();
         }
+        angle += 0.001;
 
         canvas.clear();
         framebuffer.update(None, &framedata, (WIDTH*4) as usize).expect("Texture update");
